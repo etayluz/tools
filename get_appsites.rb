7e584fdefc4c828973826e1702e3923bf518f4dd
Etay Luz <etayluz@gmail.com>
2014-12-16 00:04:44 -0500

    Save work

--- get_appsites.rb
+++ get_appsites.rb
@@ -6,6 +6,8 @@ require 'uri'
 CSV.foreach("./apps.csv", headers: true) do |row|
   puts row[1]
   doc = Nokogiri::HTML(open(row[1]))
+
+
   array = doc.css('div.app-links a').map { |link| link['href'] }
   array.uniq!
   array.each { |url| 
@@ -26,8 +28,17 @@ end
 #   end
 # end
 
-def get_host_without_www(url)
-  url = "http://#{url}" if URI.parse(url).scheme.nil?
-  host = URI.parse(url).host.downcase
-  host.start_with?('www.') ? host[4..-1] : host
-end
+# def get_host_without_www(url)
+#   url = "http://#{url}" if URI.parse(url).scheme.nil?
+#   host = URI.parse(url).host.downcase
+#   host.start_with?('www.') ? host[4..-1] : host
+# end
+
+# ENGLISH ONLY
+  # array = doc.css('li.language').map { |text| text.text }
+  # lang = array[0]
+  # # puts lang
+  # if (lang == "Language: English")
+ 	#  puts lang
+  # end
+  # next
