23c858f1dbeae5b2013b3d99973de4af336f8cbe
Etay Luz <etayluz@gmail.com>
2014-12-17 00:23:55 -0500

    Save work

--- test.rb
+++ test.rb
@@ -6,23 +6,48 @@ require 'domainatrix'
 require 'open-uri'
 # doc = Nokogiri::HTML(open("http://itenantonline.com"))
 # array = doc.css('div.app-links a').map { |link| 
-url = 'http://app-roved.com'
-html_string = open(url){|f|f.read}
-email_addresses = html_string.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i)
-puts email_addresses
-hrefs = []
-if email_addresses.nil?
-	doc = Nokogiri::HTML(html_string)
-	hrefs = doc.css("a").map do |link|
-		href = link.attr("href") 
-		if ((!href.include? "https://") && !href.nil? && !href.empty? && (!href.downcase.include? ".png") && (!href.downcase.include? "#"))
-		  url + '/' + href
+
+class EtayClass
+ 	def self.test
+	    url = 'http://app-roved.com'
+		html_string = open(url){|f|f.read}
+		# puts html_string
+		r = Regexp.new(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/)     
+		emails = html_string.scan(r).uniq
+		puts emails[0]
+		hrefs = []
+		if emails.size == 0
+			doc = Nokogiri::HTML(html_string)
+			hrefs = doc.css("a").map do |link|
+				href = link.attr("href")
+				if ((!href.include? "https://") && !href.nil? && !href.empty? && (!href.downcase.include? ".png") && (!href.downcase.include? "#"))
+				  url + '/' + href
+				end
+			end.compact.uniq
+			# STDOUT.puts(hrefs.join("\n"))
 		end
-	end.compact.uniq
-	puts hrefs
-	# STDOUT.puts(hrefs.join("\n"))
+		puts hrefs
+		hrefs.each do |url|
+			self.load(url)
+			# puts email_addresses
+		end
+	end
+
+  # A simple wrapper around the *nix cal command.
+  	def self.load(url)
+		html_string = open(url){|f|f.read}
+		r = Regexp.new(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/)     
+		emails = html_string.scan(r).uniq
+		if emails.size > 0
+			puts emails[0]
+		end
+	end
 end
-puts hrefs
+
+# puts EtayClass.test
+EtayClass.test
+
+
 
 # puts hrefs
 
